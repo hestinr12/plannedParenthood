c1cc6005c92d52654be418b79c0ba71bd1ff7940
Prashant Sridhar <psridha1@andrew.cmu.edu>
2014-02-22 12:22:14 -0500

    updated schema to add required fields and author refs

--- models/Question.js
+++ models/Question.js
@@ -1,15 +1,40 @@
 var mongoose = require('mongoose');
 
 var AnswerSchema = new mongoose.Schema({
-	text : 			String
-	author : 		Schema.Types.ObjectId
-	approved : 		Boolean
-	time_posted : 	Date
+	text : {
+		type : String,
+		required : true
+	}
+	author : {
+		type: Schema.Types.ObjectId,
+		required : true,
+		ref : 'User'
+	}
+	approved : {
+		type : Boolean,
+		required : true
+	}
+	time_posted : {
+		type : Date,
+		required : true
+	}
 })
 
 var QuestionSchema = new mongoose.Schema({
-	text : 		String
-	author : 	Schema.Types.ObjectId
-	answers : 	[AnswerSchema]
-	tags : 		[String]	
+	text : { 
+		type : String,
+		required : true,
+		validate : validate('len', 0, 160)
+	} 
+	author : {
+		type: Schema.Types.ObjectId,
+		required = true,
+		ref : 'User'
+	}
+	time_posted : {
+		type : Date,
+		required : true
+	}
+	answers : [AnswerSchema]
+	tags : [String]	
 })
