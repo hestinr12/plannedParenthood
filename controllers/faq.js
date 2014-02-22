419bad68e86e428093c04d97261017edb9490187
Chris Leaf <chris@thinkero.us>
2014-02-22 12:26:26 -0500

    Base line FAQ

--- controllers/faq.js
+++ controllers/faq.js
@@ -1,6 +1,46 @@
 
 exports.index = function(req, res) {
-  res.render('faq', {
-    title: 'Frequently Asked Questions'
-  });
+//  var user = req.user; 
+  var user = {type: "staff"};
+  var questions  =  [
+    {
+    "author": "",
+    "text"  : "Question 1",
+    "approved": true,
+    "tags": [], 
+    "answers": [ 
+      { 
+      "author": "",
+      "text"  : "Answer 1 to Q1",
+      "approved": true,
+    }, 
+    { 
+      "author": "",
+      "text"  : "Answer 2 to Q1",
+      "approved": false,
+    }, 
+    { 
+      "author": "",
+      "text"  : "Answer 3 to Q1",
+      "approved": true,
+    }
+    ]
+  },
+  { 
+    "author": "",
+    "text"  : "Question 2",
+    "approved": true,
+    "tags": [], 
+    "answers": [] 
+  }
+  ];
+  if (user.type === "staff"){
+    res.render('faq-staff', {
+      title: 'Frequently Asked Questions',
+      questions: questions });
+  }else{ // Normal student
+    res.render('faq', {
+      title: 'Frequently Asked Questions',
+      questions: questions });
+  }
 };
