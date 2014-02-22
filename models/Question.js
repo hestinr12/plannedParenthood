dc17f4fddf84e83221d1fe0a5c1ea209dc4d7c9e
Prashant Sridhar <psridha1@andrew.cmu.edu>
2014-02-22 12:11:35 -0500

    changed author to objectid and added mongoose

--- models/Question.js
+++ models/Question.js
@@ -1,13 +1,15 @@
+var mongoose = require('mongoose');
+
 var AnswerSchema = new mongoose.Schema({
 	text : 			String
-	author : 		Number
+	author : 		Schema.Types.ObjectId
 	approved : 		Boolean
 	time_posted : 	Date
 })
 
 var QuestionSchema = new mongoose.Schema({
 	text : 		String
-	author : 	Number
+	author : 	Schema.Types.ObjectId
 	answers : 	[AnswerSchema]
 	tags : 		[String]	
 })
