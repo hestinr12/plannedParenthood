31759ac910ebd99db66114890145b1e11938947b
Prashant Sridhar <psridha1@andrew.cmu.edu>
2014-02-22 15:46:40 -0600

    down with facebook

--- models/User.js
+++ models/User.js
@@ -6,11 +6,11 @@ var userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String,
 
-  facebook: { type: String, unique: true, sparse: true },
+  /*facebook: { type: String, unique: true, sparse: true },
   twitter: { type: String, unique: true, sparse: true },
   google: { type: String, unique: true, sparse: true },
   github: { type: String, unique: true, sparse: true },
-  tokens: Array,
+  */tokens: Array,
 
   type: { type: String, enum: ['student', 'staff'] },
 
