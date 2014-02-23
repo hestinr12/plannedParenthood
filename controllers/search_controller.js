20b411d23af3c0b07dbde56a8bb544281ffb75f9
chris <chrisj.leaf@gmail.com>
2014-02-22 22:52:39 -0600

    Fixed stuff

--- controllers/search_controller.js
+++ controllers/search_controller.js
@@ -2,9 +2,23 @@ var Question = require('../models/Question');
 
 exports.searchQuestion = function(req, res, next) { 
   var query = req.query.query;
+  var user = req.user;
+
   Question.textSearch(query, function(err, question) {
     results = question.results.map ( function(curr, index, thisArg) {
-      return curr.obj;}), 
-    res.send(results);
+      return curr.obj;
+    }); 
+    if (typeof user === "undefined" || user.type !== "staff") {
+      res.render('forum', {
+        title: 'Forum', 
+        questions: results
+      });
+    } else {
+      res.render('forum-staff', {
+        title: 'Forum', 
+        questions: results
+      });
+    }
+
   });
 }
